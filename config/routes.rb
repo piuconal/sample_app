eaad2f664536883282f5f18fb7699bbda8fe0f27
Vu Thi Ngoc <56816494+ngocvt-0484@users.noreply.github.com>
2023-08-14 10:45:10 +0700

    chapter_3_4_5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope "(:locale)", locale: /en|vi/ do
+    resources :products
+    get 'demo_partials/new'
+    get 'demo_partials/edit'
+    get 'static_pages/home'
+    get 'static_pages/help'
+    resources :users
+  end
 end
