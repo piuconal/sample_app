7b3ca8a0eb1af42b9764f7bca283bef442bb43f7
piuconal <hcboy1106@gmail.com>
2024-07-15 15:01:28 +0700

    chapter_9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   USER_PARAMS = [:name, :email, :password, :password_confirmation].freeze
+  before_save :downcase_email
 
   validates :name,
             presence: true,
@@ -18,16 +19,34 @@ class User < ApplicationRecord
             uniqueness: true
 
   has_secure_password
+  attr_accessor :remember_token
 
-  before_save :downcase_email
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost => cost
+    end
 
-  def self.digest string
-    cost = if ActiveModel::SecurePassword.min_cost
-             BCrypt::Engine::MIN_COST
-           else
-             BCrypt::Engine.cost
-           end
-    BCrypt::Password.create string, cost => cost
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def forget
+    update_column(:remember_digest, nil)
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
   end
 
   private
